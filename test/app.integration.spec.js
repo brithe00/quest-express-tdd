4c7dacb1cebc58587671dc31f8d437bd584a5cb3
brithe00 <bthellier51@gmail.com>
2020-06-18 11:59:39 +0200

    added tests to complete the challenge

--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 describe('Test routes', () => {
   it('GET / sends "Hello World" as json', (done) => {
@@ -9,6 +10,68 @@ describe('Test routes', () => {
       .expect('Content-Type', /json/)
       .then(response => {
         const expected = { message: 'Hello World!'};
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+});
+
+describe('GET /bookmarks/:id', () => {
+  const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+  beforeEach((done) => connection.query(
+    'TRUNCATE bookmark', () => connection.query(
+      'INSERT INTO bookmark SET ?', testBookmark, done
+    )
+  ));
+  it('GET /bookmarks/:id error case', (done) => {
+    request(app)
+      .get('/bookmarks/2')
+      .expect(404)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { error: 'Bookmark not found' };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+  it('GET /bookmarks/:id success case', (done) => {
+    request(app)
+      .get('/bookmarks/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { id: expect.any(Number), url: expect.any(String), title: expect.any(String) };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+});
+
+describe('Test cas d\'erreur', () => {
+  it('POST /bookmarks cas d\'erreur', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { error: 'required field(s) missing'};
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+});
+
+describe('Test cas de succès', () => {
+  beforeEach(done => connection.query('TRUNCATE bookmark', done));
+  it('POST /bookmarks cas de succès', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { id: expect.any(Number), url: 'https://jestjs.io', title: 'Jest' };
         expect(response.body).toEqual(expected);
         done();
       });
